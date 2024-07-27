5a18ecd20831594c3b2404032104aebb69878446
Fernando <fernandoribeirogomesjunior@gmail.com>
2024-07-26 21:27:23 -0300

    implementando métodos

---
+++ src/main/java/br/ufpb/fernando/amigosecreto/amigoNaoExisteException.java
@@ -0,0 +1,7 @@
+package br.ufpb.fernando.amigosecreto;
+
+public class amigoNaoExisteException extends Exception{
+    amigoNaoExisteException(String msg){
+        super((msg));
+    }
+}
--- src/main/java/br/ufpb/fernando/amigosecreto/sistemaAmigo.java
+++ src/main/java/br/ufpb/fernando/amigosecreto/sistemaAmigo.java
@@ -1,5 +1,6 @@
 package br.ufpb.fernando.amigosecreto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class sistemaAmigo{
@@ -24,8 +25,13 @@ public class sistemaAmigo{
     }
 
     public List<Mensagem> pesquisarMensagensAnonimas(){
-    //TODO
-        return List.of();
+    List<Mensagem> mensagensAnonimas = new ArrayList<>();
+    for(Mensagem mensagem : this.mensagens){
+        if (mensagem.getAnonima()){
+            mensagensAnonimas.add(mensagem);
+        }
+    }
+        return mensagensAnonimas;
     }
 
     public List<Mensagem> pesquisarTodasAsMensagens(){
@@ -33,16 +39,23 @@ public class sistemaAmigo{
         return List.of();
     }
 
-    public void configuraAmigoSecretoDe(String emailDaPessoa, String emailAmigoSorteado){
+    public void configuraAmigoSecretoDe(String emailDaPessoa, String emailAmigoSorteado) throws amigoNaoExisteException{
         //TODO
-    }
+        int cont = 0;
+        for(Amigo a: this.amigos ){
+            if (a.equals(emailDaPessoa)) {
+                a.setEmailAmigoSorteado(emailAmigoSorteado);
+                cont ++;
+            }
+            }
+            if (cont == 0){
+                throw new amigoNaoExisteException("Esse amigo não existe");
+            }
+        }
 
     public String pesquisaAmigoSecretoDe(String emailDaPessoa){
-    //TODO
-        return emailDaPessoa;
+        return "";
     }
 
+ }
 
-
-
-}
